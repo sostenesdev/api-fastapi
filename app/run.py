d3fd6222821a0a32e4ca06a205ed9a09dd5e85ae
Sóstenes Dias <sostenesdev@gmail.com>
2022-08-03 16:44:38 -0300

    add host config vars

--- app/run.py
+++ app/run.py
@@ -1,4 +1,5 @@
+from setup.config import HOST, PORT
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run("main:app", host=HOST, port=PORT, log_level="info")
