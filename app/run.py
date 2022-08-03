37409201ab5024b40626e8818eb93187eb97ee03
Sóstenes Dias <sostenesdev@gmail.com>
2022-08-03 16:50:26 -0300

    Add loglevel config

--- app/run.py
+++ app/run.py
@@ -1,5 +1,5 @@
-from setup.config import HOST, PORT
+from setup.config import HOST, PORT,LOG_LEVEL
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=HOST, port=PORT, log_level="info")
+    uvicorn.run("main:app", host=HOST, port=PORT, log_level=LOG_LEVEL)
