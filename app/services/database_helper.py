67f43670b0d56efe2365bcdeee76b8b6ae4ad93b
Sóstenes Dias <sostenesdev@gmail.com>
2022-08-08 11:43:19 -0300

    patrametrized connection string

--- app/services/database_helper.py
+++ app/services/database_helper.py
@@ -3,7 +3,7 @@ from pydal import DAL, Field
 from setup import config
 
 def define_database():
-    db = DAL("mysql://root:@localhost/testapi?set_encoding=utf8mb4", folder='dbs', auto_import=True,migrate_enabled=False, fake_migrate=False)
+    db = DAL(config.CONNECTION_STRING, folder='dbs', auto_import=True,migrate_enabled=False, fake_migrate=False)
     db.define_table('users',
                 Field('id', 'integer'), 
                 Field('name', required=True),
